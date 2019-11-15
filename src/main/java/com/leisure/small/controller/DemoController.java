14b2b04b3fc6deee4f7eb43e41ca95f5f6cff5b1
DESKTOP-GM7OTDC\13690 <kouyuanxun@sinosoft.com.cn>
2019-11-15 17:59:52 +0800

    project init

---
+++ src/main/java/com/leisure/small/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.leisure.small.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("demo")
+public class DemoController {
+
+    @RequestMapping(value = "getDemoPage", method = RequestMethod.GET)
+    public String getDemoPath(){
+        return "layout/footer";
+    }
+}
